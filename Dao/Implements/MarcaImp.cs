1dc953e0e0a7cebf2c7d5074843a1f7ac479b076
NicolasOgloblin <nicolas.ogloblin.w@gmail.com>
2024-04-25 16:20:11 -0300

    - Agrega comment : El crud es solo para Articulo y NO para Marca o Cateegoria

--- Dao/Implements/MarcaImp.cs
+++ Dao/Implements/MarcaImp.cs
@@ -39,7 +39,7 @@ namespace Dao.Implements
             }
         }
 
-       public int AgregarMarca(MarcaEntity marca)
+       public int AgregarMarca(MarcaEntity marca) //Tomar éste código y reutilizarlo para Artiuclos. Porque el crud es para Articulos y no para Marcas o Categorias
         {
             DataAccess datos = new DataAccess();
             string consulta = string.Format("insert marcas (Descripcion) values ('{0}')", marca.Descripcion); //Acá armo la consulta
